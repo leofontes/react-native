d263e101e4fdbc7f5a700d56a5548dc3d5d127c8
Leo Fontes <leofontes@gmail.com>
2017-12-13 11:04:53 -0200

    movies lista

--- Movies/app/components/MoviesList.js
+++ Movies/app/components/MoviesList.js
@@ -12,7 +12,30 @@ export default class MoviesList extends Component {
       data: []
     };
   }
-  
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
