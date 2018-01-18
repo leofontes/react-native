ba36a8cd68df7c8a5a2ae2c9fbbeac23f640fcc3
Leo Fontes <leofontes@gmail.com>
2018-01-18 21:55:58 -0200

    logo

--- MyCrewApp/MyCrewApp/app/screens/Login/LoginScreen.js
+++ MyCrewApp/MyCrewApp/app/screens/Login/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, Image } from 'react-native';
 
 import styles from './LoginScreenStyles'
 
@@ -7,6 +7,7 @@ export default class DetailScreen extends Component<{}> {
   render() {
     return (
       <View>
+        <Image source={require('../../assets/logo.png')} style={styles.loginLogo} />
         <Text>abc123</Text>
       </View>
     );
--- MyCrewApp/MyCrewApp/app/screens/Login/LoginScreenStyles.js
+++ MyCrewApp/MyCrewApp/app/screens/Login/LoginScreenStyles.js
@@ -1,5 +1,10 @@
 import {StyleSheet} from 'react-native';
 
 module.exports = StyleSheet.create({
-  
+    loginLogo: {
+      marginTop: 0,
+      marginBottom: 0,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
 });
