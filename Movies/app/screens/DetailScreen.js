52934bfd1dc207d66b2ad80881063371c2d3fb1b
Leonardo Fontes <leofontes@gmail.com>
2018-01-19 07:47:41 -0200

    changes

--- Movies/app/screens/DetailScreen.js
+++ Movies/app/screens/DetailScreen.js
@@ -55,13 +55,14 @@ export default class DetailScreen extends Component<{}> {
   }
 
   formatCurrency = (value) => {
-    var formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    });
-
-    return formatter.format(value);
+    // var formatter = new Intl.NumberFormat('en-US', {
+    //   style: 'currency',
+    //   currency: 'USD',
+    //   minimumFractionDigits: 2,
+    // });
+    //
+    // return formatter.format(value);
+    return value
   }
 
   render() {
